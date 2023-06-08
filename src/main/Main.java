71541fb1c26b954dcec096dfeb7ada7913f5566a
panzer-chan <101371138+panzer-chan@users.noreply.github.com>
2023-06-08 10:15:42 -0400

    bruh

--- src/main/Main.java
+++ src/main/Main.java
@@ -25,11 +25,26 @@ public class Main {
 	 */
 	
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+<<<<<<< Updated upstream
+=======
+	//static File save = new File("saves/output.json");
+>>>>>>> Stashed changes
 	static StringTokenizer st;
 	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		System.out.println(save.getAbsolutePath());
+		System.out.println("  _____      _          _                 _    \r\n"
+						 + " |  __ \\    (_)        | |               | |   \r\n"
+						 + " | |__) | __ _  ___ ___| |__   ___   ___ | | __\r\n"
+						 + " |  ___/ '__| |/ __/ _ \\ '_ \\ / _ \\ / _ \\| |/ /\r\n"
+						 + " | |   | |  | | (_|  __/ |_) | (_) | (_) |   < \r\n"
+						 + " |_|   |_|  |_|\\___\\___|_.__/ \\___/ \\___/|_|\\_\\\r\n"
+						 + "                                               \r\n"
+						 + "                                               ");
+		
+		
+		
+		
 	}
 
 }

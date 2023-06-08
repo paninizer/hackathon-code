a7530d3633268164ae71a3f2a77425b251c52f67
panzer-chan <101371138+panzer-chan@users.noreply.github.com>
2023-06-08 09:25:23 -0400

    storename

--- src/main/Item.java
+++ src/main/Item.java
@@ -19,16 +19,18 @@ public class Item {
 	double price;
 	Units unit;
 	String brandName;
+	String storeName;
 	boolean isSale;
 	LocalDate date;
 	
-	public Item(String name, double quantity, Units unit, double price, String brandName, boolean isSale) {
+	public Item(String name, double quantity, Units unit, double price, String brandName, boolean isSale, String store) {
 		this.name = name;
 		this.quantity = quantity;
 		this.unit = unit;
 		this.price = price;
 		this.brandName = brandName;
 		this.isSale = isSale;
+		this.storeName = store;
 		this.date = LocalDate.now();
 	}
 }
